efaa6d66923be695ec7380b2a29723b865370a71
raulwad13 <rawad13@myyahoo.com>
2023-11-06 19:27:57 +0100

    fetch

---
+++ script.js
@@ -0,0 +1,33 @@
+const characterEl = document.getElementById("character");
+const nameFilter = document.getElementById("name_filter");
+const filmFilter = document.getElementById("film_filter");
+
+// Call API.
+
+async function getCharacter(name, films) {
+  let url = "https://swapi.dev/api/people";
+
+  const response = await fetch(url);
+  const data = await response.json();
+
+  console.log(data);
+  return data.results;
+}
+getCharacter();
+
+// Print cards
+
+async function displayCharacters(name, films) {
+    // obtener los personajes filtrados
+    const characters = await getCharacter(name, films);
+  
+    // renderizar los personajes
+    for (let character of characters) {
+      const card = document.createElement("div");
+      card.classList.add("character_card");
+  
+      console.log("Nombre del personaje: " + character.name);
+    // card.innerHTML =
+  }
+}
+displayCharacters();
