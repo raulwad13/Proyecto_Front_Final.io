7fc4d3fd11ddbdc006aa55c4fb3da8087a17577a
raulwad13 <rawad13@myyahoo.com>
2023-11-06 19:51:52 +0100

    first print and basic css

--- script.js
+++ script.js
@@ -18,16 +18,28 @@ getCharacter();
 // Print cards
 
 async function displayCharacters(name, films) {
-    // obtener los personajes filtrados
-    const characters = await getCharacter(name, films);
-  
-    // renderizar los personajes
-    for (let character of characters) {
-      const card = document.createElement("div");
-      card.classList.add("character_card");
-  
-      console.log("Nombre del personaje: " + character.name);
-    card.innerHTML =
+  // obtener los personajes filtrados
+  const characters = await getCharacter(name, films);
+
+  // renderizar los personajes
+  for (let character of characters) {
+    const card = document.createElement("div");
+    card.classList.add("character_card");
+
+    //   console.log("Nombre del personaje: " + character.name);
+    card.innerHTML = `
+
+        <h2>${character.name}</h2>
+        <p>Heigth: ${character.height}</p>
+        <p>Gender: ${character.films}</p>
+        <p>Appearance: ${character.skin_color}</p>
+        <p>Created: ${character.created}</p>
+        <p>Films: ${character.films}</p>
+
+        `;
+
+
+        characterEl.appendChild(card);
   }
 }
 displayCharacters();
